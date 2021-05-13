e7fcda6c9e0b1d13f71fdc8097f00faa64de1327
Estuardo19 <84014850+Estuardo19@users.noreply.github.com>
2021-05-13 11:52:43 -0600

    Add files via upload

--- HojadeTrabajo1.py
+++ HojadeTrabajo1.py
@@ -1,5 +1,5 @@
-Peso = input("Cuál es su peso en kilogramos?")
-Estatura = input("Cuál es su estatura en metros?")
+Peso = input("¿Cuál es su peso en kilogramos?")
+Estatura = input("¿Cuál es su estatura en metros?")
 
 imc = round(float(Peso)/ float(Estatura)**2, 2)
 
